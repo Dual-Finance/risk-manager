670fc89283305bfd657b74aac29c65462a329161
Britt Cyr <cyrbritt@gmail.com>
2022-10-20 16:02:06 -0400

    Update url for devnet and mainnet rpc. Do not use the proxy anymore

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const ENVIRONMENT: string = IS_DEV ? "DEVNET" : "MAINNET";
 
 export const networkName = IS_DEV ? 'devnet.2' : 'mainnet.1';
 export const cluster = IS_DEV ? 'devnet' : 'mainnet-beta';
-export const API_URL = IS_DEV ? 'https://dual-rpc.com/devnet' : 'https://dual-rpc.com/mainnet';
+export const API_URL = IS_DEV ? 'https://solana-devnet.g.alchemy.com/v2/e5EQixWHc-n0F3JTe-ueWzKZIJDMYXTi' : 'https://solana-mainnet.g.alchemy.com/v2/GdkPP9kAPDUH_qVo8GQbUIoD32BZyAbA';
 export const DUAL_API = IS_DEV ? 'https://dev.api.dual.finance' : 'https://api.dual.finance';
 export const WEBSOCKET_URL = IS_DEV ? 'https://api.devnet.solana.com' : 'https://api.mainnet-beta.solana.com';
 export const FILLS_URL = IS_DEV ? 'ws://api.mngo.cloud:2082' : 'ws://v3.mngo.cloud:8080';

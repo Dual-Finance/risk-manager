bd1293bb9bf81925d00c8a7523526c30e55891bb
DonDuala <107803907+DonDuala@users.noreply.github.com>
2023-01-23 11:07:48 -0400

    await checking MM quotes (#125)
    * await checking MM quotes
    
    * comment

--- src/router.ts
+++ src/router.ts
@@ -52,7 +52,7 @@ export class Router {
 
   // Accepts a DIP Deposit and decides whether to send it to the mm_callback
   // or risk_manager_callback
-  route(dipDeposit: DIPDeposit): void {
+  async route(dipDeposit: DIPDeposit): Promise<void> {
     const date = new Date(dipDeposit.expirationMs);
 
     // TODO: Update this for other types of assets
@@ -61,17 +61,18 @@ export class Router {
       date.getUTCMonth() + 1
     }-${date.getUTCDate()},${dipDeposit.strikeUsdcPerToken * 1_000_000},UPSIDE,E,P`;
     console.log('++++++++++++++++++++++++++++++++++++++++++++++++++++');
-    console.log('Routing for', symbol, 'Deposit:', dipDeposit, new Date().toUTCString());
+    console.log('Routing', dipDeposit.qtyTokens, symbol, new Date().toUTCString());
 
     // This happens after sending tokens to a MM. Exit early.
     if (dipDeposit.qtyTokens === 0) {
       this.dips[
         this.dipToString(dipDeposit.expirationMs / 1_000, dipDeposit.strikeUsdcPerToken)
       ] = dipDeposit;
-      console.log('DIP Deposit quantity zero. No Rerun');
+      console.log('DIP Deposit quantity zero. Rerun');
+      this.run_risk_manager;
       return;
     }
-    this.fetchMMOrder(symbol).then(async (order) => {
+    await this.fetchMMOrder(symbol).then(async (order) => {
       // Run the risk manager if there is no MM order
       // @ts-ignore
       if (!order || Number(order.remainingQuantity) < dipDeposit.qtyTokens) {
@@ -81,8 +82,8 @@ export class Router {
             dipDeposit.strikeUsdcPerToken,
           )
         ] = dipDeposit;
-        this.run_risk_manager();
         console.log('No available MM bid', order);
+        this.run_risk_manager();
         return;
       }
 
@@ -93,7 +94,7 @@ export class Router {
       const vol = BVE_VOL_MAP.get(
         dipDeposit.splTokenName,
       ) * (1 + volSpread + Math.random() * volSpread);
-      const thresholdPrice = blackScholes(currentPrice, dipDeposit.strikeUsdcPerToken, fractionOfYear, vol, 0.01, 'call');
+      const thresholdPrice = blackScholes(currentPrice, dipDeposit.strikeUsdcPerToken, fractionOfYear, vol, rfRate, 'call');
       // @ts-ignore
       const { price } = order;
       console.log('MM price:', price, 'BVE Re-Route price:', thresholdPrice);
@@ -159,103 +160,21 @@ export class Router {
     });
   }
 
-  // Todo use this routing logic only once for reruns and new trades
   // Reads all DIP Deposits and decides whether to send it to the mm_callback
   async checkMMPrices(): Promise<void> {
+    let openPositionCount = 0;
     for (const dipDeposit of Object.values(this.dips)) {
       if (dipDeposit.qtyTokens > 0) {
-        const date = new Date(dipDeposit.expirationMs);
-        // TODO: Update this for other types of assets
-        const symbol = `${dipDeposit.splTokenName},USDC,${date.getUTCFullYear()}-${
-          date.getUTCMonth() + 1
-        }-${date.getUTCDate()},${dipDeposit.strikeUsdcPerToken * 1_000_000},UPSIDE,E,P`;
-        console.log('######################################################################');
-        console.log('Checking MM Quotes vs ', symbol, 'Deposit:', dipDeposit, new Date().toUTCString());
-
-        this.fetchMMOrder(symbol).then(async (order) => {
-          // @ts-ignore
-          if (!order || Number(order.remainingQuantity) < dipDeposit.qtyTokens) {
-            this.dips[
-              this.dipToString(
-                dipDeposit.expirationMs / 1_000,
-                dipDeposit.strikeUsdcPerToken,
-              )
-            ] = dipDeposit;
-            console.log('No available MM bid', order);
-            return;
-          }
-
-          const currentPrice = await getPythPrice(
-            new PublicKey(tokenToSplMint(dipDeposit.splTokenName)),
-          );
-          const fractionOfYear = (
-            dipDeposit.expirationMs - Date.now()) / (365 * 24 * 60 * 60 * 1_000);
-          const vol = BVE_VOL_MAP.get(dipDeposit.splTokenName)
-            * (1 + volSpread + Math.random() * volSpread);
-          const thresholdPrice = blackScholes(
-            currentPrice, dipDeposit.strikeUsdcPerToken, fractionOfYear, vol, rfRate, 'call');
-          // @ts-ignore
-          const { price } = order;
-          console.log('MM price:', price, 'BVE price:', thresholdPrice);
-          const userPremium = price * dipDeposit.qtyTokens;
-          if (userPremium < minExecutionPremium) {
-            // If user premium is too small don't bother spamming MM
-            console.log('Not routing too small of a trade:', userPremium, minExecutionPremium);
-            this.dips[
-              this.dipToString(
-                dipDeposit.expirationMs / 1_000,
-                dipDeposit.strikeUsdcPerToken,
-              )
-            ] = dipDeposit;
-            return;
-          }
-
-          if (thresholdPrice > price || !(thresholdPrice > 0)) {
-            // If the price is worse than the BVE, then do not use the MM, treat it
-            // like there is no MM bid.
-            console.log('Not routing to MM due to price:', thresholdPrice, price);
-            this.dips[
-              this.dipToString(
-                dipDeposit.expirationMs / 1_000,
-                dipDeposit.strikeUsdcPerToken,
-              )
-            ] = dipDeposit;
-            return;
-          }
-
-          const clientOrderId = `clientOrderId${Date.now()}`;
-          const side = 'SELL';
-          const quantity = dipDeposit.qtyTokens;
-          // @ts-ignore
-          const secret = apiSecret.default;
-
-          const request = `clientOrderId=${clientOrderId}&symbol=${symbol}&price=${price}&quantity=${quantity}&side=${side}`;
-          const calculatedHash = crypto
-            .createHmac('SHA256', secret)
-            .update(Buffer.from(request))
-            .digest('hex');
-
-          const data = {
-            symbol,
-            price,
-            quantity,
-            side,
-            clientOrderId,
-            signature: calculatedHash,
-          };
-
-          console.log('Creating api order to sell', data);
-          const response = await fetch(`${DUAL_API}/orders/createorder`, {
-            method: 'post',
-            headers: {
-              'Content-Type': 'application/json',
-              'X-MBX-APIKEY': PROTCOL_API_KEY,
-            },
-            body: JSON.stringify(data),
-          });
-          console.log('API response', await response.json());
-        });
+        openPositionCount++;
+        await this.route(dipDeposit);
       }
+    }
+    // On startup run risk manager when there is no position
+    if (openPositionCount === 0) {
+      console.log('No Positions. Run Risk Manager', new Date().toUTCString());
+      this.run_risk_manager();
+    } else {
+      console.log('Open DIP Positions', openPositionCount);
     }
   }
 
